3e6b578d1983790a0e2bceecbde01bdaa90e38dd
esandine <esandine@stuy.edu>
2016-10-06 23:51:51 -0400

    Addded redirect

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, url_for, redirect
 from utils import check
 app=Flask(__name__)
 
 @app.route("/")
-def hello():
+def login():
     return render_template("form.html")
 
 @app.route("/authenticate/", methods =['POST'])
@@ -17,7 +17,7 @@ def auth():
 @app.route("/register/", methods = ['POST'])
 def reg():
     check.register(request.form)
-    return render_template("form.html")
+    return redirect(url_for("login"))
 
 if __name__=='__main__':
     app.debug=True
